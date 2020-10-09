1c673996c4ee5abbf5f7c2e289e444c7ff001d90
cnridley <cnridley1@gmail.com>
2020-10-09 03:33:28 +0000

    added shopping bag functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,19 +1,24 @@
-""" This is what's known as a context processor.
-And its purpose is to make this dictionary available to all templates across the entire application
-Much like you can use request.user in any template
-due to the presence of the built-in request context processor.
-"""
-
-""" 'bag.contexts.bag_contents' this is added to the settings.py to make it work. """
-
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
